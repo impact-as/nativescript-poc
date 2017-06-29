725a13e739245ba1791a375684b9a0e9b852849e
Filip Bech-Larsen <filip@FBB-MBPRO.local>
2017-06-29 21:02:01 +0200

    moved basket to drawer

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -2,15 +2,13 @@ import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
-import { HomeComponent, LoginComponent, SearchComponent, CategoryComponent, BasketComponent } from './pages';
-import { DrawerComponent } from './drawer.component';
+import { HomeComponent, LoginComponent, SearchComponent, CategoryComponent } from './pages';
 
 const routes: Routes = [
     { path: "", component: HomeComponent },
     { path: "login", component: LoginComponent },
     { path: "search", component: SearchComponent },
-    { path: "category", component: CategoryComponent },
-    { path: "basket", component: BasketComponent }
+    { path: "category", component: CategoryComponent }
 ];
 
 @NgModule({
--- platforms/ios/impactbynativescript/app/tab-navigation.component.ts
+++ platforms/ios/impactbynativescript/app/tab-navigation.component.ts
@@ -3,12 +3,11 @@ import { Component } from '@angular/core';
 @Component({
     selector:'tab-navigation',
     template:`
-        <FlexboxLayout backgroundColor="black" flexDirection="row" justifyContent="space-between">
+        <FlexboxLayout backgroundColor="black" flexDirection="row" justifyContent="space-around">
             <Button text="home" color="white" nsRouterLink="/"></Button>
             <Button text="login" color="white" nsRouterLink="/login"></Button>
             <Button text="category" color="white" nsRouterLink="/category"></Button>
             <Button text="search" color="white" nsRouterLink="/search"></Button>
-            <Button text="basket" color="white" nsRouterLink="/basket"></Button>
         </FlexboxLayout>
     `
 })
