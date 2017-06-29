95f248745e2ded3302c786bef993999c0e8ac2ba
simsoll <simon@soellingvraa.dk>
2017-06-29 23:27:49 +0200

    Added spinner on details page and enabling tapping on related products

--- app/pages/detail.component.ts
+++ app/pages/detail.component.ts
@@ -10,39 +10,41 @@ import { FilterService } from "../services/filter.service";
   template: `
  <ScrollView>
     <StackLayout orientation="vertical">
-        <StackLayout class="container border" orientation="vertical">
-            <Image class="image" [src]="'https://bogogide.godkend.dk' + product?.PrimaryImageUrl + '&width=500&height=500'"></Image>
-        </StackLayout>
-
-        <StackLayout class="container" orientation="vertical">
-            <Label class="price" [text]="product?.PricesSanitized.ActualPrice"></Label>
-            <Label class="title" textWrap="true" [text]="product?.Name"></Label>
-            
-            <button class="button" text="Add to basket"></button>
-        </StackLayout>
-
-
-        <WrapLayout width="100%">
-            <StackLayout width="100%" height="80" *ngFor="let product of relatedProducts">
-            <GridLayout width="100%" columns="60, *, 80" rows="25, 20, 20" style="padding:0px;" (tap)="onTap($event, product.Id)">
-                <Image (tap)="onTap($event, product.id)" width="80" rowSpan="3" row="0" col="0" [src]="'https://bogogide.godkend.dk' + product?.PrimaryImageUrl + '&width=100&height=100'" class="image" width="50" height="50"></Image>
-                <Label row="0" col="1" [text]="product.Name" class="label"></Label>
-                <Label row="1" col="1" [text]="product.ItemDistributor" class="label mini"></Label>
-                <Label row="2" col="1" [text]="product.PricesSanitized.ActualPrice" class="label mini"></Label>
-            </GridLayout>
+        <StackLayout *ngIf="product" orientation="vertical">
+            <StackLayout class="container border" orientation="vertical">
+                <Image class="image" [src]="'https://bogogide.godkend.dk' + product?.PrimaryImageUrl + '&width=500&height=500'"></Image>
             </StackLayout>
-        </WrapLayout>
+
+            <StackLayout class="container" orientation="vertical">
+                <Label class="price" [text]="product?.PricesSanitized.ActualPrice"></Label>
+                <Label class="title" textWrap="true" [text]="product?.Name"></Label>
+                
+                <button class="button" text="Add to basket"></button>
+            </StackLayout>
 
 
+            <WrapLayout width="100%">
+                <StackLayout width="100%" height="80" *ngFor="let product of relatedProducts">
+                <GridLayout width="100%" columns="60, *, 80" rows="25, 20, 20" style="padding:0px;" (tap)="onTap($event, product.Id)">
+                    <Image (tap)="onTap($event, product.id)" width="80" rowSpan="3" row="0" col="0" [src]="'https://bogogide.godkend.dk' + product?.PrimaryImageUrl + '&width=100&height=100'" class="image" width="50" height="50"></Image>
+                    <Label row="0" col="1" [text]="product.Name" class="label"></Label>
+                    <Label row="1" col="1" [text]="product.ItemDistributor" class="label mini"></Label>
+                    <Label row="2" col="1" [text]="product.PricesSanitized.ActualPrice" class="label mini"></Label>
+                </GridLayout>
+                </StackLayout>
+            </WrapLayout>
+        </StackLayout>
+        <ActivityIndicator [busy]="isLoading" [visibility]="isLoading ? 'visible' : 'collapse'" row="1" horizontalAlignment="center" verticalAlignment="center"></ActivityIndicator>        
     </StackLayout>
-
  </ScrollView>
   `
 })
 export class DetailComponent {
-    product:any;
-    relatedProducts:any[] = [];
-    id: number;
+    public product:any;
+    public relatedProducts:any[] = [];
+    public isLoading = false;       
+    private categoryId: string;
+
     constructor(
         private pageRoute: ActivatedRoute, 
         private filterService: FilterService, 
@@ -51,30 +53,31 @@ export class DetailComponent {
     }
 
     ngOnInit() {
-        this.pageRoute.params
-        .subscribe(params => {
-            this.filterService.getCategoryProducts(params['categoryId'])
+        this.pageRoute.params.subscribe(params => {
+            this.categoryId = params['categoryId'];
+
+            //force repaint when tapping related products
+            this.isLoading = true;
+            this.product = null;
+
+            this.filterService.getCategoryProducts(this.categoryId)
                 .subscribe(products => {
                     this.product = products.find(a => a.Id === params['productId']);
                     products.forEach(item => {
                         this.relatedProducts.push(item);
                     });
+                    this.isLoading = false;
                 });
         });
    }
 
-    public onTap(args: GestureEventData, id:string) {
-        let product = null;
-        // this.productService.products.subscribe(items=> {
-        //     product = items.find(a => a.Id === id);
-        //     this.routerExtensions.navigate(["/detail", product.Id], {
-        //         transition: {
-        //             name: "slide",
-        //             duration: 300,
-        //             curve: "linear"
-        //         }
-        //     });
-        // });
+    public onTap(args: GestureEventData, id: string) {
+        this.routerExtensions.navigate(["/detail", this.categoryId, id], {
+            transition: {
+                name: "slide",
+                duration: 300,
+                curve: "linear"
+            }
+        });        
     }
-
 }
