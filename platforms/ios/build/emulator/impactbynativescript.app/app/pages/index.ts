d3522ff695a7d66631c914eaf5b43869f6ba4a6d
Filip Bech-Larsen <filip@FBB-MBPRO.local>
2017-06-29 22:00:52 +0200

    remove platform from sourcecontrol

--- platforms/ios/build/emulator/impactbynativescript.app/app/pages/index.ts
+++
@@ -1,6 +0,0 @@
-export { HomeComponent } from './home.component'; 
-export { LoginComponent } from './login.component'; 
-export { BasketComponent } from './basket.component';
-export { SearchComponent } from './search.component';
-export { CategoryComponent } from './category.component';
-export { FindComponent } from './find.component';
--- platforms/ios/build/impactbynativescript.build/Debug-iphonesimulator/impactbynativescript.build/Objects-normal/i386/main.d
+++
@@ -1,9 +0,0 @@
-dependencies: \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/main.m \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/TNSDebugging.h \
-  /Users/filip/Desktop/impactbynativescript/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
