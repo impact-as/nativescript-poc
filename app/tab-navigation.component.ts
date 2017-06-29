d5566dc57c23c74a0f446fed1839a458b4771e58
simsoll <simon@soellingvraa.dk>
2017-06-29 23:42:55 +0200

    Increased tab navigation icons and added active link styling

--- app/tab-navigation.component.ts
+++ app/tab-navigation.component.ts
@@ -4,11 +4,11 @@ import { Component } from '@angular/core';
     selector:'tab-navigation',
     template:`
         <FlexboxLayout backgroundColor="black" flexDirection="row" justifyContent="space-around">
-            <Image src="~/images/home.png" width="20" height="20" nsRouterLink="/"></Image>
-            <Image src="~/images/login.png" width="20" height="20" nsRouterLink="/login"></Image>
-            <Image src="~/images/category.png" width="20" height="20" nsRouterLink="/category"></Image>
-            <Image src="~/images/search.png" width="20" height="20" nsRouterLink="/search"></Image>
-            <Image src="~/images/find.png" width="20" height="20" nsRouterLink="/find"></Image>
+            <Image src="~/images/home.png" width="40" height="40" nsRouterLink="/" nsRouterLinkActive="active-link" [nsRouterLinkActiveOptions]="{exact: true}"></Image>
+            <Image src="~/images/login.png" width="40" height="40" nsRouterLink="/login" nsRouterLinkActive="active-link"></Image>
+            <Image src="~/images/category.png" width="40" height="40" nsRouterLink="/category" nsRouterLinkActive="active-link"></Image>
+            <Image src="~/images/search.png" width="40" height="40" nsRouterLink="/search" nsRouterLinkActive="active-link"></Image>
+            <Image src="~/images/find.png" width="40" height="40" nsRouterLink="/find" nsRouterLinkActive="active-link"></Image>
         </FlexboxLayout>
     `
 })
