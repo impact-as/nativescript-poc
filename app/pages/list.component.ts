ee8c0fda8b72afa98fd1c224f8f481661f75bae6
Christian Hartøft-Nielsen <hartoeft@gmail.com>
2017-06-30 12:02:19 +0200

    listview missing update products

--- app/pages/list.component.ts
+++ app/pages/list.component.ts
@@ -16,39 +16,39 @@ import "rxjs/add/operator/do";
 @Component({
   selector: "list",
   template: `
-
-    <StackLayout orientation="vertical">
-        <GridLayout tkExampleTitle tkToggleNavButton>
-            <RadListView [items]="facetResult" 
-                          class="list-group" 
-                          selectionBehavior="Press" 
-                          multipleSelection="true"
-                          (itemSelected)="updateQuery($event)"
-                          (itemDeselected)="updateQuery($event)">
+        <GridLayout rows="200, *">
+            
+            <RadListView row="0" [items]="facetResult" 
+                        class="list-group" 
+                        selectionBehavior="Press" 
+                        multipleSelection="true"
+                        (itemSelected)="updateQuery($event)"
+                        (itemDeselected)="updateQuery($event)">
+                
                 <ng-template tkListItemTemplate let-item="item" let-i="index" let-odd="odd" let-even="even">
                     <StackLayout width="100%" columns="*" rows="*" style="padding:0px;" ios:class="iosListItemStackLayout" android:class="androidListItemStackLayout">
                         <Label ios:class="iosNameLabel" android:class="androidNameLabel" [text]="item.Query.Name"></Label>
                     </StackLayout>
                 </ng-template>
             </RadListView>
-        </GridLayout>
+            
 
-        <ListView [items]="products" (itemTap)="onItemTap($event)" class="list-group">
-            <ng-template let-product="item" let-i="index" let-odd="odd" let-even="even">
-                <GridLayout width="100%" columns="60, *, 80" rows="25,20,20" style="padding:0px;">
-                    <Image width="80" rowSpan="3" row="0" col="0" [src]="'https://bogogide.godkend.dk' + product.PrimaryImageUrl + '&width=100&height=100'" class="image" width="50" height="50"></Image>
-                    <Label row="0" col="1" [text]="product.Name" class="label"></Label>
-                    <Label row="1" col="1" [text]="product.ItemDistributor + '/' + product.ItemPublisher" class="label mini"></Label>
-                    <Label row="2" col="1" [text]="product.PricesSanitized.RetailPriceLabel" class="label mini"></Label>
-                    <Label row="0" col="2" [text]="product.PricesSanitized.ActualPriceAmount" class="label price"></Label>
+            <ListView row="1" [items]="products" (itemTap)="onItemTap($event)" class="list-group">
+                <ng-template let-product="item" let-i="index" let-odd="odd" let-even="even">
+                    <GridLayout width="100%" columns="60, *, 80" rows="25,20,20" style="padding:0px;">
+                        <Image width="80" rowSpan="3" row="0" col="0" [src]="'https://bogogide.godkend.dk' + product.PrimaryImageUrl + '&width=100&height=100'" class="image" width="50" height="50"></Image>
+                        <Label row="0" col="1" [text]="product.Name" class="label"></Label>
+                        <Label row="1" col="1" [text]="product.ItemDistributor + '/' + product.ItemPublisher" class="label mini"></Label>
+                        <Label row="2" col="1" [text]="product.PricesSanitized.RetailPriceLabel" class="label mini"></Label>
+                        <Label row="0" col="2" [text]="product.PricesSanitized.ActualPriceAmount" class="label price"></Label>
 
-                    <Label class="button" height="50" width="40" (tap)="addtobasket(product.Id)" text="+" col="2" row="1"></Label>
+                        <Label class="button" height="50" width="40" (tap)="addtobasket(product.Id)" text="+" col="2" row="1"></Label>
 
-                </GridLayout>
-            </ng-template>
-        </ListView>
-    <ActivityIndicator [busy]="isLoading" [visibility]="isLoading ? 'visible' : 'collapse'" row="1" horizontalAlignment="center" verticalAlignment="center"></ActivityIndicator>
-    </StackLayout>
+                    </GridLayout>
+                </ng-template>
+            </ListView>
+        <ActivityIndicator [busy]="isLoading" [visibility]="isLoading ? 'visible' : 'collapse'" row="1" horizontalAlignment="center" verticalAlignment="center"></ActivityIndicator>
+    </GridLayout>
   `
 })
 export class ListComponent {
@@ -72,6 +72,7 @@ export class ListComponent {
 
             this.filter.subscribe(data => {
                 this.products = data.Products;
+
                 this.facets = data.Facets.filter(data => {
                     return data.Control === 'Dropdown' && data.Key !== 'ItemGroup';
                 });
@@ -81,7 +82,6 @@ export class ListComponent {
                 this.facetResult = this.facets[1].FacetResults;
                 console.log('facets', this.facetResult);
             });
-
         });
     }
  
@@ -136,12 +136,19 @@ export class ListComponent {
     }
 
     public updateProductList(query: any) {
-        console.log('query', query);
+        this.isLoading = true;
 
         this.filter = this.filterService.getCategoryFilter(this.categoryId, query);
 
+
         this.filter.subscribe(data => {
-            console.log('new data', data);
+            console.log('data', data);
+            this.products = data.Products;
+
+            this.facets = data.Facets.filter(data => {
+                return data.Control === 'Dropdown' && data.Key !== 'ItemGroup';
+            });
+            this.isLoading = false;
         });
     }
 
